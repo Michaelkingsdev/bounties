9378bd3563b3cb5737273c69556692940026b7e1
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2026-01-25 21:47:13 +0100

    async effect updated

---
+++ hooks/use-async-effect.ts
@@ -0,0 +1,33 @@
+import * as React from "react"
+
+/**
+ * Hook to run an async effect.
+ * 
+ * @param effect The async function to run.
+ * @param deps The dependency array.
+ * @param destructor Optional destructor function.
+ */
+export function useAsyncEffect(
+    effect: () => Promise<void | (() => void | undefined)>,
+    deps: React.DependencyList
+) {
+    React.useEffect(() => {
+        let cleanup: void | (() => void | undefined)
+        let mounted = true
+
+        const run = async () => {
+            if (mounted) {
+                cleanup = await effect()
+            }
+        }
+
+        run()
+
+        return () => {
+            mounted = false
+            if (cleanup && typeof cleanup === "function") {
+                cleanup()
+            }
+        }
+    }, deps)
+}
